fc5730a11a0f674634178ab9710e9316cc4a69bd
Hari Pulugurta <charanp@gmail.com>
2018-03-18 01:04:45 -0400

    added new chart

--- min_max.py
+++ min_max.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
 import mplleaflet
 import pandas as pd
-from collections import namedtuple
+import numpy as np
+import calendar as cl
+
 
 def leaflet_plot_stations(binsize, hashid):
     df = pd.read_csv('BinSize_d{}.csv'.format(binsize))
@@ -12,42 +14,55 @@ def leaflet_plot_stations(binsize, hashid):
     plt.scatter(lons, lats, c='r', alpha=0.7, s=200)
     return mplleaflet.show()
 
+
 def weather_pattern():
     df=pd.read_csv("fb441e62df2d58994928907a91895ec62c2c42e6cd075c2700843b89.csv")
     df=df.drop(['ID'],axis=1)
-    val1=df[df['Element']=="TMAX"].groupby(['Date','Element']).max()
-    val2=df[df['Element']=="TMIN"].groupby(['Date','Element']).min()
 
-    val1=val1.add_suffix('_Max').reset_index()
-    val2=val2.add_suffix('_Max').reset_index()
+    # Determine C,Day of Year
+    df["Data_Value"]=df["Data_Value"]/10
+    df[["Year","Month","Day"]] =df["Date"].str.split("-",expand=True).astype('int')
+    df["Day_Of_Year"]=(pd.to_datetime(df["Date"])).dt.dayofyear
 
-    val3=pd.concat([val1,val2])
-    val4=val3.pivot(index='Date',columns='Element',values='Data_Value_Max')
+    # Remove leap year data
+    leap_df=df.query('Month ==2 & Day ==29')
+    df=df.drop(leap_df.index.values)
+
+    # Max temperatures
+    df_max=df[df['Element']=="TMAX"].groupby(['Month', 'Day','Element']).agg({'Data_Value':'max'}).reset_index()
+    df_min=df[df['Element']=="TMIN"].groupby(['Month', 'Day','Element']).agg({'Data_Value':'min'}).reset_index()
+    df=pd.concat([df_max,df_min]).reset_index()
+
+    # Create TMAX and TMIN columns and add day of the year column
+    df= df.groupby(['Month', 'Day', 'Element'])['Data_Value'].min().unstack('Element')
+    df.insert(0,"DayOfYear",range(1,len(df)+1))
     
-    df_new=pd.DataFrame()
-    df_new=val4
+    # Get x-tick positon at the change of the month
+    x_ticks=[ v['DayOfYear'] for k,v in df.iterrows() if k[1]==1 ]
+    x_minor_ticks= [ (val+x_ticks[idx-1])/2 for idx,val in enumerate(x_ticks) if idx>0 ]
+    x_minor_ticks.append( ( max(x_ticks)+365)/2 )
+    x_minor_labels=[ month[:3] for month in cl.month_name if month!='' ]
+ 
+    max_values=df['TMAX'].values
+    min_values=df['TMIN'].values
+    x_values=range(1,len(max_values)+1)
 
-    print(df_new.columns.names())
+    # plot
+    FIG, AX = plt.subplots(nrows=1, ncols=1)
+    FIG.canvas.draw()
+    AX.plot(x_values,max_values)
+    AX.plot(x_values,min_values)
+    AX.set_ylim([-40,50])
+    AX.set_xlim([1,365])
 
-    # indexed_df = val1.set_index(['Date'])
-    #plt.subplots(nrows=1, ncols=1)
-    
-    # sr=pd.Series(val4["TMAX"])
+    AX.tick_params(axis='x',length=0,which='minor')
+    AX.set_xticks(x_ticks)
+    AX.set_xticks(x_minor_ticks,minor=True)
+    AX.set_xticklabels([])
+    AX.set_xticklabels(x_minor_labels,minor=True)
 
-    #plt.plot(df_new["Date"])
-    
-    #plt.show()
-    
-    #df1=val1.val3,columns='Element',values='Data_Value')
-
-    # Creaval1year,month,day
-    #df[["Year","Month","Day"]] =df["Date"].str.split("-",expand=True).astype('int')
-
-    # Exclude leap year
-    #leap_df=df.query('Month ==2 & Day ==29')
-    #df=df.drop(leap_df.index.values)
-    #print(df1)
-
+    plt.fill_between(x_values,max_values,min_values)
+    plt.show()
 
 # leaflet_plot_stations(400,'fb441e62df2d58994928907a91895ec62c2c42e6cd075c2700843b89')
 weather_pattern()
