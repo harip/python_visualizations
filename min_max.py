9490c0fa01d1229240f85047e92deba68dac753c
Hari Pulugurta <charanp@gmail.com>
2018-03-18 04:48:56 -0400

    improved perf

--- min_max.py
+++ min_max.py
@@ -5,61 +5,46 @@ import numpy as np
 import calendar as cl
 from matplotlib.pyplot import figure, show, cm
 
-
-def leaflet_plot_stations(binsize, hashid):
-    df = pd.read_csv('BinSize_d{}.csv'.format(binsize))
-    station_locations_by_hash = df[df['hash'] == hashid]
-    lons = station_locations_by_hash['LONGITUDE'].tolist()
-    lats = station_locations_by_hash['LATITUDE'].tolist()
-    plt.figure(figsize=(8,8))
-    plt.scatter(lons, lats, c='r', alpha=0.7, s=200)
-    return mplleaflet.show()
-
-
-def weather_pattern():
-    df=pd.read_csv("fb441e62df2d58994928907a91895ec62c2c42e6cd075c2700843b89.csv")
+def weather_pattern(file_name):
+    df=pd.read_csv('data/C2A2_data/BinnedCsvs_d400/{}.csv'.format(file_name))
     df=df.drop(['ID'],axis=1)
     
-    # Determine C,Day of Year
-    df["Data_Value"]=df["Data_Value"]/10
+    df_max1=df[df['Element']=="TMAX"].groupby(['Date','Element']).agg({'Data_Value':'max'}).reset_index()
+    df_min2=df[df['Element']=="TMIN"].groupby(['Date','Element']).agg({'Data_Value':'min'}).reset_index()
+    df=pd.concat([df_max1,df_min2]).reset_index()    
+    
+    df= df.groupby(['Date', 'Element'])['Data_Value'].min().unstack('Element').reset_index() 
+    df["TMAX"]=df["TMAX"]/10
+    df["TMIN"]=df["TMIN"]/10
     df[["Year","Month","Day"]] =df["Date"].str.split("-",expand=True).astype('int')
     df["Day_Of_Year"]=(pd.to_datetime(df["Date"])).dt.dayofyear
-
+    
     # Remove leap year data
     leap_df=df.query('Month ==2 & Day ==29')
     df=df.drop(leap_df.index.values)
-    
-    df_2015=df[df["Year"]==2015]
+
+    df_2015=df[df["Year"]==2015].reindex()
     df=df[df["Year"]<2015]
+    df=df.drop(['Date','Year'],axis=1)
 
-    # Max temperatures
-    df_max=df[df['Element']=="TMAX"].groupby(['Month', 'Day','Element']).agg({'Data_Value':'max'}).reset_index()
-    df_min=df[df['Element']=="TMIN"].groupby(['Month', 'Day','Element']).agg({'Data_Value':'min'}).reset_index()
-    df=pd.concat([df_max,df_min]).reset_index()    
-
-    # Create TMAX and TMIN columns and add day of the year column
-    df= df.groupby(['Month', 'Day', 'Element'])['Data_Value'].min().unstack('Element')
-    df.insert(0,"Day_Of_Year",range(1,len(df)+1))
+    # Merge df again based on day of the year
+    df=df.groupby(['Day_Of_Year']).agg({'TMAX':'max','TMIN':'min'}).reset_index()
 
     # Get 2015 record breaking data
     df_record=pd.DataFrame(columns=['X','Y'])
-    for k,v in df.iterrows():
+    for k,v in df_2015.iterrows():
         # Get 2015 values for TMAX for this day and month
-        tmax_values=df_2015[(df_2015["Day_Of_Year"]==v["Day_Of_Year"]) & (df_2015["Element"]=='TMAX')]["Data_Value"].values
-        max_tmax_value=0 #max(tmax_values)
+        tmax_value_2000_2014=df[(df["Day_Of_Year"]==v["Day_Of_Year"])]["TMAX"].values[0]
+        tmin_value_2000_2014=df[(df["Day_Of_Year"]==v["Day_Of_Year"])]["TMIN"].values[0]
 
-        # Get 2015 values for TMIN for this day and month
-        tmin_values=df_2015[(df_2015["Day_Of_Year"]==v["Day_Of_Year"]) & (df_2015["Element"]=='TMIN')]["Data_Value"].values
-        min_tmin_value=0 #min(tmin_values)
+        if (v["TMAX"]>tmax_value_2000_2014):
+            df_record.loc[len(df_record)]=[v["Day_Of_Year"],v["TMAX"]]
 
-        if (max_tmax_value>v["TMAX"]):
-            df_record.loc[len(df_record)]=[v["Day_Of_Year"],max_tmax_value]
-
-        if (min_tmin_value<v["TMIN"]):
-            df_record.loc[len(df_record)]=[v["Day_Of_Year"],min_tmin_value]        
+        if (v["TMIN"]<tmin_value_2000_2014):
+            df_record.loc[len(df_record)]=[v["Day_Of_Year"],v["TMIN"]]      
 
     # Get x-tick positon at the change of the month
-    x_ticks=[ v['Day_Of_Year'] for k,v in df.iterrows() if k[1]==1 ]
+    x_ticks=[ v[1]['Day_Of_Year'] for v in df_2015.iterrows() if v[1]['Day']==1 ]
     x_minor_ticks= [ (val+x_ticks[idx-1])/2 for idx,val in enumerate(x_ticks) if idx>0 ]
     x_minor_ticks.append( ( max(x_ticks)+365)/2 )
     x_minor_labels=[ month[:3] for month in cl.month_name if month!='' ]
@@ -77,7 +62,9 @@ def weather_pattern():
     extent=(1, 365, -40, 50), alpha=1,aspect='auto')
     AX.plot(x_values,max_values,zorder=1,linewidth=0)
     AX.plot(x_values,min_values,zorder=1,linewidth=0)
-    AX.scatter(df_record["X"].values,df_record["Y"].values,zorder=2,c='r',label="Record 2015 Temperature")
+
+    scatter_colors=[ 1 for val in df_record["X"].values ]
+    AX.scatter(df_record["X"].values,df_record["Y"].values,zorder=2, color='b',label="Record 2015 Temperature", alpha=0.75)
     AX.set_ylim([-40,50])
     AX.set_xlim([1,365])
 
@@ -102,4 +89,4 @@ def weather_pattern():
     plt.show()
 
 # leaflet_plot_stations(400,'fb441e62df2d58994928907a91895ec62c2c42e6cd075c2700843b89')
-weather_pattern()
+weather_pattern('fb441e62df2d58994928907a91895ec62c2c42e6cd075c2700843b89')
